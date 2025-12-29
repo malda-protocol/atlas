e6b90e08e0c8233b68ef162ab3aad13fd30dedfc
Andrzej Broński <andrzej1_1@o2.pl>
2025-12-29 21:56:07 +0100

    Add support for Base node.

---
+++ base/mainnet/generate-jwt-token.sh
@@ -0,0 +1,12 @@
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+mkdir -p "${SCRIPT_DIR}/jwttoken"
+if [[ ! -f "${SCRIPT_DIR}/jwttoken/jwt.hex" ]]
+then
+  openssl rand -hex 32 | tr -d "\n" | tee > "${SCRIPT_DIR}/jwttoken/jwt.hex"
+  echo "Token generated!"
+else
+  echo "${SCRIPT_DIR}/jwttoken/jwt.hex already exists!"
+fi
